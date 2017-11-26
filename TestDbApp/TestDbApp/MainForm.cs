0207a4ffb2477027b1cc007c80f507c24c4f5888
alevya <alexv47@mail.ru>
2017-11-26 21:39:54 +0300

    commit

--- TestDbApp/TestDbApp/MainForm.cs
+++ TestDbApp/TestDbApp/MainForm.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using TestDbApp.Common;
 using TestDbApp.EntityFrameworkBinding;
 using TestDbApp.Model;
 
@@ -11,8 +9,6 @@ namespace TestDbApp
 {
     public partial class MainForm : Form
     {
-        private int _ageEmployee;
-
         public MainForm()
         {
             InitializeComponent();
@@ -22,77 +18,9 @@ namespace TestDbApp
             bindSrc_DepartmentToEmployee.DataMemberChanged += BindSrcDepartmentToEmployeeOnDataMemberChanged;
             bindSrc_DepartmentToEmployee.CurrentItemChanged += BindSrcDepartmentToEmployeeOnCurrentItemChanged;
             bindSrc_DepartmentToEmployee.CurrentChanged += BindSrcDepartmentToEmployeeOnCurrentChanged;
-          
-            dtp_DateBirth.ValueChanged += DtpDateBirthOnValueChanged;
         }
 
-        private void EntityDataSourceOrgOnDataError(object sender, DataErrorEventArgs args)
-        {
-            string msgError;
-            var inExc = args.Exception.InnerException;
-            if (inExc?.InnerException != null)
-            {
-                msgError = inExc.InnerException.Message;
-                
-            }
-            else
-            {
-                msgError = args.Exception.Message;
-            }
-            MessageBox.Show(this, msgError, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            args.Handled = true;
-        }
-
-        private void BindSrcDepartmentToEmployeeOnCurrentItemChanged(object sender, EventArgs eventArgs)
-        {
-            
-        }
-
-        private void DtpDateBirthOnValueChanged(object sender, EventArgs eventArgs)
-        {
-            //var diff = new DifferenceDate(dtp_DateBirth.Value, DateTime.Now);
-            //tb_Age.Text = Convert.ToString(diff.Years);
-        }
-
-        private void BindSrcDepartmentToEmployeeOnCurrentChanged(object sender, EventArgs eventArgs)
-        {
-            var currentEmpl = bindSrc_DepartmentToEmployee.Current as Employee;
-            if(currentEmpl == null) return;
-
-            //var dtBirth = currentEmpl.DateOfBirth;
-            //var diff = new DifferenceDate(dtBirth, DateTime.Now);
-            //tb_Age.Text = Convert.ToString(diff.Years);
-        }
-
-        private void TvDepartmentOnAfterSelect(object sender, TreeViewEventArgs treeViewEventArgs)
-        {
-            var selectedNode = tv_Department.SelectedNode;
-            Department selDepartment = selectedNode.Tag as Department;
-            if(selDepartment == null) return;
-
-            var employees = new List<Employee>();
-            var l = entityDataSource_Org.EntitySets["Departments"].Cast<Department>().ToList();
-
-            bindSrc_DepartmentToEmployee.Clear();
-            GetEmployees(employees, selDepartment, l);
-            if(!employees.Any()) return;
-
-            bindSrc_DepartmentToEmployee.DataSource = employees;
-            dgv_EmployeeToDepartment.DataSource = bindSrc_DepartmentToEmployee;
-        }
-
-        private static void GetEmployees(List<Employee> employees, Department dep, IEnumerable<Department> departments)
-        {
-            var ds = from Department d in departments
-                where d.ParentDepartmentID == dep.DepartmentId
-                select d;
-
-            employees.AddRange(dep.Employees);
-            foreach (var item in ds)
-            {
-                GetEmployees(employees, item, departments);
-            }
-        }
+        #region Events
 
         private void OnLoad(object sender, EventArgs eventArgs)
         {
@@ -111,69 +39,22 @@ namespace TestDbApp
             //---
         }
 
-        private void _bindingEmployeeDetails(object dataSource)
+        private void EntityDataSourceOrgOnDataError(object sender, DataErrorEventArgs args)
         {
-            if (dataSource == null) return;
-
-            ec_FirstName.DataBindings.Add(new Binding("Value", dataSource, ec_FirstName.AttributeName, true));
-            ec_SurName.DataBindings.Add(new Binding("Value", dataSource, ec_SurName.AttributeName, true));
-            ec_Patronymic.DataBindings.Add(new Binding("Value", dataSource, ec_Patronymic.AttributeName,true));
-            ec_Position.DataBindings.Add(new Binding("Value", dataSource, ec_Position.AttributeName, true));
-            ec_DocNumber.DataBindings.Add(new Binding("Value", dataSource, ec_DocNumber.AttributeName, true));
-            ec_DocSeries.DataBindings.Add(new Binding("Value", dataSource, ec_DocSeries.AttributeName, true));
-            dtp_DateBirth.DataBindings.Add(new Binding("Value", dataSource, "DateOfBirth", true));
-            tb_Age.DataBindings.Add(new Binding("Text", dataSource, "Age", true));
-
-            //Binding ComboBox на вкладке <Структура предприятия> 
-            var bindDepToEmpl =
-                new Binding("SelectedValue", dataSource, "DepartmentID", true)
-                {
-                    DataSourceUpdateMode = DataSourceUpdateMode.OnPropertyChanged,
-                    ControlUpdateMode = ControlUpdateMode.OnPropertyChanged
-                };
-            cb_DepartmentToEmployee.DataBindings.Add(bindDepToEmpl);
-            bindSrc_DepartmentToEmployee.DataMemberChanged += BindSrcDepartmentToEmployeeOnDataMemberChanged;
-        }
-
-        private void BindSrcDepartmentToEmployeeOnDataMemberChanged(object sender, EventArgs eventArgs)
-        {
-            
-        }
-
-        private void _populateTreeView()
-        {
-            var list = tv_Department.Tag as IEnumerable<Department>;
-            if(list == null) return;
-
-            var departments = list as IList<Department> ?? list.ToList();
-            var rootNodes = departments.Where(department => department.ParentDepartmentID == Guid.Empty ||
-                                                            department.ParentDepartmentID == null);
-            foreach (var rootNode in rootNodes)
+            string msgError;
+            var inExc = args.Exception.InnerException;
+            if (inExc?.InnerException != null)
             {
-                var node = GetTreeNode(rootNode, departments);
-                node.Expand();
-                tv_Department.Nodes.Add(node);
+                msgError = inExc.InnerException.Message;
+
             }
-        }
-
-        private static TreeNode GetTreeNode(Department row, IEnumerable<Department> list)
-        {
-            Guid.TryParse(row.DepartmentId.ToString(), out Guid nodeID);
-            var node = new TreeNode
+            else
             {
-                Text = Convert.ToString(row.Name),
-                Tag = row
-            };
-            var res = list.Where(department => department.ParentDepartmentID == nodeID);
-            foreach (var item in res)
-            {
-                var chNode = GetTreeNode(item, list);
-                chNode.Text = Convert.ToString(item.Name);
-                node.Nodes.Add(chNode);
+                msgError = args.Exception.Message;
             }
-            return node;
+            MessageBox.Show(this, msgError, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            args.Handled = true;
         }
-
 
         private void _btnSave_Click(object sender, EventArgs e)
         {
@@ -196,6 +77,111 @@ namespace TestDbApp
 
         }
 
-        //internal int AgeCurrentEmployee 
+        private void BindSrcDepartmentToEmployeeOnCurrentItemChanged(object sender, EventArgs eventArgs)
+        {
+
+        }
+
+        private void BindSrcDepartmentToEmployeeOnCurrentChanged(object sender, EventArgs eventArgs)
+        {
+            var currentEmpl = bindSrc_DepartmentToEmployee.Current as Employee;
+            if (currentEmpl == null) return;
+
+        }
+
+        private void TvDepartmentOnAfterSelect(object sender, TreeViewEventArgs treeViewEventArgs)
+        {
+            var selectedNode = tv_Department.SelectedNode;
+            Department selDepartment = selectedNode.Tag as Department;
+            if (selDepartment == null) return;
+
+            var employees = new List<Employee>();
+            var l = entityDataSource_Org.EntitySets["Departments"].Cast<Department>().ToList();
+
+            bindSrc_DepartmentToEmployee.Clear();
+            GetEmployees(employees, selDepartment, l);
+            if (!employees.Any()) return;
+
+            bindSrc_DepartmentToEmployee.DataSource = employees;
+            dgv_EmployeeToDepartment.DataSource = bindSrc_DepartmentToEmployee;
+        }
+
+        #endregion
+
+        #region Methods
+
+        private static void GetEmployees(List<Employee> employees, Department dep, IEnumerable<Department> departments)
+        {
+            var ds = from Department d in departments
+                where d.ParentDepartmentID == dep.DepartmentId
+                select d;
+
+            employees.AddRange(dep.Employees);
+            foreach (var item in ds)
+            {
+                GetEmployees(employees, item, departments);
+            }
+        }
+
+
+
+        private void _bindingEmployeeDetails(object dataSource)
+        {
+            if (dataSource == null) return;
+
+            ec_FirstName.DataBindings.Add(new Binding("Value", dataSource, ec_FirstName.AttributeName, true));
+            ec_SurName.DataBindings.Add(new Binding("Value", dataSource, ec_SurName.AttributeName, true));
+            ec_Patronymic.DataBindings.Add(new Binding("Value", dataSource, ec_Patronymic.AttributeName, true));
+            ec_Position.DataBindings.Add(new Binding("Value", dataSource, ec_Position.AttributeName, true));
+            ec_DocNumber.DataBindings.Add(new Binding("Value", dataSource, ec_DocNumber.AttributeName, true));
+            ec_DocSeries.DataBindings.Add(new Binding("Value", dataSource, ec_DocSeries.AttributeName, true));
+            dtp_DateBirth.DataBindings.Add(new Binding("Value", dataSource, "DateOfBirth", true));
+            tb_Age.DataBindings.Add(new Binding("Text", dataSource, "Age", true));
+
+            //Binding ComboBox на вкладке <Структура предприятия> 
+            var bindDepToEmpl =
+                new Binding("SelectedValue", dataSource, "DepartmentID", true)
+                {
+                    DataSourceUpdateMode = DataSourceUpdateMode.OnPropertyChanged,
+                    ControlUpdateMode = ControlUpdateMode.OnPropertyChanged
+                };
+            cb_DepartmentToEmployee.DataBindings.Add(bindDepToEmpl);
+        }
+
+        private void _populateTreeView()
+        {
+            var list = tv_Department.Tag as IEnumerable<Department>;
+            if (list == null) return;
+
+            var departments = list as IList<Department> ?? list.ToList();
+            var rootNodes = departments.Where(department => department.ParentDepartmentID == Guid.Empty ||
+                                                            department.ParentDepartmentID == null);
+            foreach (var rootNode in rootNodes)
+            {
+                var node = GetTreeNode(rootNode, departments);
+                node.Expand();
+                tv_Department.Nodes.Add(node);
+            }
+        }
+
+        private static TreeNode GetTreeNode(Department row, IEnumerable<Department> list)
+        {
+            Guid.TryParse(row.DepartmentId.ToString(), out Guid nodeID);
+            var node = new TreeNode
+                       {
+                           Text = Convert.ToString(row.Name),
+                           Tag = row
+                       };
+            var res = list.Where(department => department.ParentDepartmentID == nodeID);
+            foreach (var item in res)
+            {
+                var chNode = GetTreeNode(item, list);
+                chNode.Text = Convert.ToString(item.Name);
+                node.Nodes.Add(chNode);
+            }
+            return node;
+        }
+
+        #endregion
     }
 }
